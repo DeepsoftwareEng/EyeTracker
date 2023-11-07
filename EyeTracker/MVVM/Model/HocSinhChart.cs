c43809604bb149daa6160bbe6d27bfa457cf8cb7
DeepsoftwareEng <Deepit2507@gmail.com>
2023-11-07 17:55:06 +0700

    ViewChartModule

---
+++ EyeTracker/MVVM/Model/HocSinhChart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EyeTracker.MVVM.Model
+{
+    class HocSinhChart
+    {
+        public double Mypoia { get; set; }
+        public string MaLop { get; set; }
+    }
+}
--- EyeTracker/MVVM/View/ViewChartUI.xaml.cs
+++ EyeTracker/MVVM/View/ViewChartUI.xaml.cs
@@ -1,17 +1,14 @@
-﻿using System;
+﻿using EyeTracker.MVVM.Model;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace EyeTracker.MVVM.View
 {
@@ -20,9 +17,101 @@ namespace EyeTracker.MVVM.View
     /// </summary>
     public partial class ViewChartUI : Page
     {
+        private string Magv;
+        public SeriesCollection mypoia { get; set; }
+        private DataConnection dc = new DataConnection();
+        private SqlCommand cmd;
+        private List<Lop> classes = new List<Lop>();
+        private List<HocSinhChart> students = new List<HocSinhChart>();
         public ViewChartUI(string magv)
         {
             InitializeComponent();
+            Magv = magv;
+            AddData();
+            DataContext = this;
+        }
+        private void GetClasses()
+        {
+            string query = "Select * from Lop where GVCN = @magv";
+            if (dc.GetConnection().State == System.Data.ConnectionState.Closed)
+                dc.GetConnection().Open();
+            cmd = new SqlCommand(query, dc.GetConnection());
+            try
+            {
+                cmd.Parameters.AddWithValue("@magv", Magv);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Lop temp = new Lop();
+                    temp.MaLop = reader.GetString(0);
+                    temp.TenLop = reader.GetString(1);
+                    temp.MaGV = Magv;
+                    classes.Add(temp);
+                    Button btn = new Button();
+                    btn.Content = temp.TenLop;
+                    btn.Tag = temp.MaLop;
+                    btn.Click += ChooseClass_Click;
+                    ClassListBtn.Children.Add(btn);
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            dc.GetConnection().Close();
+            foreach(var i in classes)
+            {
+                GetStudent(i.MaLop);
+            }
+        }
+
+        private void ChooseClass_Click(object sender, RoutedEventArgs e)
+        {
+            SeriesCollection seriesViews = new SeriesCollection();
+            string malop = (sender as Button).Tag.ToString();
+            var SameClass = from HocSinhChart in students where HocSinhChart.MaLop == malop select HocSinhChart;
+            List<HocSinhChart> sameclass = SameClass.ToList();
+            var result = sameclass.Select(s => s.Mypoia).Distinct();
+            foreach (var i in result)
+            {
+                PieSeries t = new PieSeries();
+                t.Title = i.ToString();
+                t.Values = new ChartValues<ObservableValue> { new ObservableValue(sameclass.Where(s => s.Mypoia == i).Count())};
+                t.LabelPoint = chartPoint => string.Format(i.ToString());
+                t.DataLabels = true;
+                seriesViews.Add(t);
+            }
+            MypoiaChart.IsEnabled = false;
+            MypoiaChart.IsEnabled = true;
+            MypoiaChart.Series = seriesViews;
+        }
+
+        private void GetStudent(string ClassCode)
+        {
+            string query = "select * from HocSinh where MaLop = @malop";
+            if (dc.GetConnection().State == System.Data.ConnectionState.Closed)
+                dc.GetConnection().Open();
+            cmd = new SqlCommand(query, dc.GetConnection());
+            try
+            {
+                cmd.Parameters.AddWithValue("@malop", ClassCode);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    HocSinhChart temp = new HocSinhChart();
+                    temp.Mypoia = reader.GetDouble(5);
+                    temp.MaLop = ClassCode;
+                    students.Add(temp);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            dc.GetConnection().Close();
+        }
+        private void AddData()
+        {
+            GetClasses();
         }
     }
 }

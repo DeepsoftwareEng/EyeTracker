a1c18b5dc8891fd1f105be763e0798e4c03b669d
DeepsoftwareEng <Deepit2507@gmail.com>
2023-12-22 22:55:47 +0700

    DataReport

--- EyeTracker/MVVM/View/ReportUI.xaml.cs
+++ EyeTracker/MVVM/View/ReportUI.xaml.cs
@@ -1,9 +1,13 @@
-﻿using DocumentFormat.OpenXml.Wordprocessing;
+﻿using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Wordprocessing;
 using EyeTracker.MVVM.Model;
 using Microsoft.Data.SqlClient;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -21,11 +25,18 @@ namespace EyeTracker.MVVM.View
         private SqlCommand cmd;
         private List<HocSinh> hocSinhs = new();
         private List<Lop> lops = new();
-        private GiaoVien giaovien = new GiaoVien(); 
+        private GiaoVien giaovien = new GiaoVien();
+        private string ReportType;
+        private string filepath;
+        private static string binFolderPath = System.IO.Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory);
+        private static string projectFolderPath = Directory.GetParent(binFolderPath).FullName;
+        private static string fix = projectFolderPath.Remove(projectFolderPath.Length - 9);
+        private static string LogFolderPath = System.IO.Path.Combine(fix, "Log");
         public ReportUI(string magv)
         {
             InitializeComponent();
             this.magv = magv;
+            filepath = LogFolderPath + $"{magv}.txt";
             LoadData();
             LoadFunction();
         }
@@ -34,10 +45,95 @@ namespace EyeTracker.MVVM.View
             StudentDetailBtn.Click += ViewStudentReport;
             ClassDetail.Click += ViewClassReport;
             MypoiaDetailBtn.Click += ViewMypoiaReport;
+            ExportBtn.MouseLeftButtonDown += ExportBtn_MouseLeftButtonDown;
+            ExportBtn.MouseEnter += ExportBtn_MouseEnter;
+            ExportBtn.MouseLeave += ExportBtn_MouseLeave;
         }
 
+        private void ExportBtn_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            SolidColorBrush brush = new SolidColorBrush(System.Windows.Media.Color.FromRgb(169,169,169));
+            (sender as System.Windows.Controls.Border).Background = brush;
+        }
+        private void ExportBtn_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            SolidColorBrush brush = new SolidColorBrush(System.Windows.Media.Color.FromRgb(211, 211, 211));
+            (sender as System.Windows.Controls.Border).Background = brush;
+        }
+        private void ExportBtn_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if(ReportDtg.Items.Count == 0)
+            {
+                MessageBox.Show("Bảng thống kê trống");
+            }
+            else
+            {
+                SaveFileDialog savefile = new SaveFileDialog();
+                savefile.DefaultExt = ".xlsx";
+                savefile.Filter = "Excel Files|*xlsx;*xls;*xlsm";
+                savefile.FileName = $"DataReport - {ReportType} - {DateTime.Now.Date}.xlsx";
+                if (savefile.ShowDialog() == true)
+                {
+                    try
+                    {
+                        var workbook = new XLWorkbook();
+
+                        workbook.Style.Font.FontSize = 11;
+                        workbook.Style.Font.FontName = "Times New Roman";
+
+                        var sheet1 = workbook.Worksheets.Add("Sheet1");
+                        int rowWrite = 1;
+
+                        Type t;
+                        PropertyInfo[] p;
+                        IXLCell cell;
+                        cell = sheet1.Cell(rowWrite, 1);
+                        cell.Value = "Attandance workers list";
+                        cell.Style.Font.Bold = true;
+                        cell.Style.Font.FontSize = 14;
+                        rowWrite++;
+                        for (int col = 0; col < ReportDtg.Columns.Count; col++)
+                        {
+                            cell = sheet1.Cell(rowWrite, col + 1);
+                            cell.Value = ReportDtg.Columns[col].Header.ToString();
+                            cell.Style.Fill.BackgroundColor = XLColor.Aqua;
+                            cell.Style.Font.Bold = true;
+                            cell.Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+                        }
+                        rowWrite++;
+
+                        foreach (var item in ReportDtg.Items)
+                        {
+                            for (int col = 0; col < ReportDtg.Columns.Count; col++)
+                            {
+                                TextBlock Value = ReportDtg.Columns[col].GetCellContent(item) as TextBlock;
+                                cell = sheet1.Cell(rowWrite, col + 1);
+                                cell.Value = Value.Text;
+                                cell.Style.Fill.BackgroundColor = XLColor.FromHtml("#F2F2F2");
+                                cell.Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+                            }
+                            rowWrite++;
+                        }
+                        rowWrite++;
+                        sheet1.Columns().AdjustToContents();
+                        workbook.SaveAs(savefile.FileName);
+                        MessageBox.Show("Done", "Message", MessageBoxButton.OK);
+                        workbook.Dispose();
+                        using (StreamWriter sw = File.AppendText(filepath))
+                        {
+                            sw.WriteLine($"{magv} - {DateTime.Now}: Xuat danh sach {ReportType}");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
         private void ViewMypoiaReport(object sender, RoutedEventArgs e)
         {
+            ReportType = "Độ cận thị";
             SolidColorBrush brush = new SolidColorBrush(System.Windows.Media.Color.FromRgb(169, 169, 169));
             SolidColorBrush brush2 = new SolidColorBrush(System.Windows.Media.Color.FromRgb(211, 211, 211));
             (sender as Button).Background = brush;
@@ -70,10 +166,15 @@ namespace EyeTracker.MVVM.View
                 var sameclass = hocSinhs.Where(s => s.MaLop == i.MaLop).ToList();
                 ReportDtg.Items.Add(new { TenLop = i.TenLop, GVCN = giaovien.TenGV, Amount =  hocSinhs.Count - hocSinhs.Where(s=>s.DoCanThi ==0).Count()});
             }
+            using (StreamWriter sw = File.AppendText(filepath))
+            {
+                sw.WriteLine($"{magv} - {DateTime.Now}: xem thong ke do can thi");
+            }
         }
 
         private void ViewClassReport(object sender, RoutedEventArgs e)
         {
+            ReportType = "Lớp";
             SolidColorBrush brush = new SolidColorBrush(System.Windows.Media.Color.FromRgb(169, 169, 169));
             SolidColorBrush brush2 = new SolidColorBrush(System.Windows.Media.Color.FromRgb(211, 211, 211));
             (sender as Button).Background = brush;
@@ -112,10 +213,15 @@ namespace EyeTracker.MVVM.View
             {
                 ReportDtg.Items.Add(new {MaLop = i.MaLop, TenLop = i.TenLop,GVCN = giaovien.TenGV, Amount = hocSinhs.Where(s => s.MaLop == i.MaLop).Count() });
             }
+            using (StreamWriter sw = File.AppendText(filepath))
+            {
+                sw.WriteLine($"{magv} - {DateTime.Now}: xem thong ke lop");
+            }
         }
 
         private void ViewStudentReport(object sender, RoutedEventArgs e)
         {
+            ReportType = "Học sinh";
             SolidColorBrush brush = new SolidColorBrush(System.Windows.Media.Color.FromRgb(169,169,169));
             SolidColorBrush brush2 = new SolidColorBrush(System.Windows.Media.Color.FromRgb(211, 211, 211));
             (sender as Button).Background = brush;
@@ -173,6 +279,10 @@ namespace EyeTracker.MVVM.View
             foreach(var i in hocSinhs)
             {
                 ReportDtg.Items.Add(new {MaHocSinh = i.MaHocSinh, HoTen = i.HoTen, NgaySinh = i.NgaySinh.ToString(), NamNhapHoc = i.NamNhapHoc.ToString(), DiaChi = i.DiaChi, DoCanThi = i.DoCanThi, Lop = lops.Where(c => c.MaLop == i.MaLop).Select(c=> c.TenLop).FirstOrDefault()});
+            }
+            using (StreamWriter sw = File.AppendText(filepath))
+            {
+                sw.WriteLine($"{magv} - {DateTime.Now}: xem thong ke hoc sinh");
             }
         }
 
